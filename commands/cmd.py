f70450b1dc54e50c28ec681147b7b009faa9736b
n42 <quentin.lieumont@gmail.com>
2019-04-17 11:59:02 +0200

    start perms json

--- commands/cmd.py
+++ commands/cmd.py
@@ -4,6 +4,7 @@ The file where all commands are builds
 @date: 01/04/2019
 @author: Quentin Lieumont
 """
+from sources import get_json
 
 
 class Context:
@@ -24,6 +25,26 @@ class Command:
     @classmethod
     def act(cls):
         return cls.__name__.lower()
+
+    @classmethod
+    def _get_perms(cls) -> dict:
+        """
+        get witch role/user are allowed to use this command
+        :return: the dict of allowed role, allowed user
+        """
+        json = get_json("commands/perms.json")
+        rl = []
+        ul = []
+
+        for role in json["role"]:
+            if role["this_role"] == cls.act():  # < --- missing inherit
+                rl.append(role)
+
+        for user in json["user"]:
+            if user["this_user"] == cls.act():  # < --- missing inherit
+                rl.append(user)
+
+        return {"role": rl, "user": ul}
 
     @classmethod
     def test_perm(cls, context: Context) -> str:
